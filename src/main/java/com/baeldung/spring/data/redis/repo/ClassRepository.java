9a3c13d771e939b329de2f664151a148f01bfa01
hieplq <lequanghiep74@gmail.com>
2017-10-31 09:30:00 +0700

    Add git ignore

--- src/main/java/com/baeldung/spring/data/redis/repo/ClassRepository.java
+++ src/main/java/com/baeldung/spring/data/redis/repo/ClassRepository.java
@@ -1,12 +1,12 @@
 package com.baeldung.spring.data.redis.repo;
 
 import com.baeldung.spring.data.redis.model.StudentClass;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClassRepository
-        extends CrudRepository<StudentClass, String>
+        extends JpaRepository<StudentClass, String>
 {
     StudentClass findOneByName(String name);
 }
--- src/main/java/com/baeldung/spring/data/redis/repo/StudentRepository.java
+++ src/main/java/com/baeldung/spring/data/redis/repo/StudentRepository.java
@@ -1,12 +1,12 @@
 package com.baeldung.spring.data.redis.repo;
 
 import com.baeldung.spring.data.redis.model.Student;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository
-        extends CrudRepository<Student, String>
+        extends JpaRepository<Student, String>
 {
     Student findOneByName(String name);
 }
